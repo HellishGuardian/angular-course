9ae056acec37b2135aa2d500ddc3f57608f4b6ac
Иван <bondband2010@gmail.com>
2022-01-31 15:56:43 +0200

    Added backend data to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,36 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Inject, Injector, OnInit} from '@angular/core';
-import {Course} from './model/course';
-import {Observable} from 'rxjs';
-import {AppConfig, CONFIG_TOKEN} from './config';
-import {COURSES} from '../db-data';
-import {CoursesService} from './courses/courses.service';
-import {createCustomElement} from '@angular/elements';
-import {CourseTitleComponent} from './course-title/course-title.component';
-
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DoCheck,
+  Inject,
+  Injector,
+  OnInit,
+} from "@angular/core";
+import { Course } from "./model/course";
+import { Observable } from "rxjs";
+import { AppConfig, CONFIG_TOKEN } from "./config";
+import { COURSES } from "../db-data";
+import { CoursesService } from "./courses/courses.service";
+import { createCustomElement } from "@angular/elements";
+import { CourseTitleComponent } from "./course-title/course-title.component";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-    courses: Course[] = COURSES;
+  constructor(private http: HttpClient) {}
 
-    coursesTotal = this.courses.length;
+  ngOnInit(): void {
+    const params = new HttpParams()
+    .set('page', '1')
+    .set('pageSize', '10');
 
-    constructor(
-        private coursesService: CoursesService,
-        @Inject(CONFIG_TOKEN) private config: AppConfig,
-        private injector: Injector) {
-
-    }
-
-    ngOnInit() {
-
-        const htmlElement = createCustomElement(CourseTitleComponent, {injector:this.injector});
-
-        customElements.define('course-title', htmlElement);
-
-    }
-
-    onEditCourse() {
-
-            this.courses[1].category = 'ADVANCED';
-
-    }
-
-    save(course: Course) {
-        this.coursesService.saveCourse(course)
-            .subscribe(
-                () => console.log('Course Saved!')
-            );
-    }
-
-
+    this.courses$ = this.http.get<Course[]>("/api/courses", {params});
+  }
 }
